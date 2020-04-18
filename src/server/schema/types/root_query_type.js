422c472f245e28f9ccaa5c8ff01ce0e9d69afd09
Pedro Magalhães <pedromagalhaes@users.noreply.github.com>
2020-04-18 06:45:37 +0100

    Update root_query_type.js

--- src/server/schema/types/root_query_type.js
+++ src/server/schema/types/root_query_type.js
@@ -3,11 +3,6 @@ const graphql = require('graphql')
 
 const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql
 const UserType = require('./user_type')
-const SongType = require('./song_type')
-const LyricType = require('./lyric_type')
-
-const Lyric = mongoose.model('lyric')
-const Song = mongoose.model('song')
 
 const RootQueryType = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -16,26 +11,6 @@ const RootQueryType = new GraphQLObjectType({
       type: UserType,
       resolve(parentValue, args, req) {
         return req.user
-      }
-    },
-    songs: {
-      type: new GraphQLList(SongType),
-      resolve() {
-        return Song.find({})
-      }
-    },
-    song: {
-      type: SongType,
-      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-      resolve(parentValue, { id }) {
-        return Song.findById(id)
-      }
-    },
-    lyric: {
-      type: LyricType,
-      args: { id: { type: new GraphQLNonNull(GraphQLID) } },
-      resolve(parnetValue, { id }) {
-        return Lyric.findById(id)
       }
     }
   }
